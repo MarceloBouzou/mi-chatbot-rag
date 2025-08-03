6e357640f073192f8a584cd3b6827c90dba057ed
MarceloBouzou <90725970+MarceloBouzou@users.noreply.github.com>
2025-08-02 23:08:50 -0300

    Update index.js
    volvemos al modelo 1.5 flash latest

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const port = process.env.PORT || 3000; // Usa el puerto de OnRender o 3000 en lo
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 
 // Versión actual (flotante)
-//const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
 // Versión futura (fija y estable, si quisieras usarla)
-const model = genAI.getGenerativeModel({ model: "gemini-pro" }); // Reemplazarías 'latest' por el número de versión
+//const model = genAI.getGenerativeModel({ model: "gemini-pro" }); // Reemplazarías 'latest' por el número de versión
 
 let knowledge = ''; // Variable para guardar el conocimiento del archivo
 
